6fd01501966094488aca918a6c13dd035c072c71
Hass <seennd1@gmail.com>
2022-05-08 22:18:40 +0300

    all done

--- algo.py
+++ algo.py
@@ -7,21 +7,20 @@ import numpy as np
 
 def  AgreedWay(l,x):
     a=x
-    np.trim_zeros(l)
-    print(l)
+    l= np.trim_zeros(l)
     a+= l.count(0)
-    print(a)
-    l=[i - 1 for i in l]
-    if(len(list(filter(lambda x: (x >= 0), l))) != 0):
-        AgreedWay(l,a)
+    L=[i - 1 for i in l]
+    l = [0 if i < 0 else i for i in L]
+    if(len(list(filter(lambda x: (x > 0), l))) != 1):
+        return AgreedWay(l,a)
     else:
-        
+
         return a
 
 
 
 
-l = [1,0,2,0,2,0]
+l = [3,0,2,0,4]
 print(AgreedWay(l,0))
 
 
