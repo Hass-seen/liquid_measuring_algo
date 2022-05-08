5756c29d6ec32528b2d0791462197ae1ff22b411
Hass <seennd1@gmail.com>
2022-05-08 22:25:00 +0300

    added documentation

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,1 +1,17 @@
 This is an algorythem for the mesuering of the water level on an alititude list.
+
+
+
+Reqiuermnets: 
+
+		certifi==2021.10.8
+		numpy==1.22.3
+		wincertstore==0.2
+
+
+
+
+i used recursion to increas efficiancy
+
+
+each step of the workflow is writtet as a comment in the code 
--- algo.py
+++ algo.py
@@ -6,16 +6,16 @@ import numpy as np
 
 
 def  AgreedWay(l,x):
-    a=x
-    l= np.trim_zeros(l)
-    a+= l.count(0)
-    L=[i - 1 for i in l]
-    l = [0 if i < 0 else i for i in L]
-    if(len(list(filter(lambda x: (x > 0), l))) != 1):
+    a=x  #a is the counter of the aria of the body of water 
+    l= np.trim_zeros(l) #removes leading and trailing Zeros from the list
+    a+= l.count(0)  # counts the number of Zeros left and adds it to the aria
+    L=[i - 1 for i in l]  # decriments every eliment of the list by 1
+    l = [0 if i < 0 else i for i in L]  # replaces every negative number with a Zero
+    if(len(list(filter(lambda x: (x > 0), l))) != 1): #if there is only one non zero value left the recursion stops
         return AgreedWay(l,a)
     else:
 
-        return a
+        return a #returns the aria of the body of water
 
 
 
